08c8ee40f5886babe76f74a9c33c99da7663ee0d
Drew Finstrom <drew@finstrom.us>
2015-06-12 10:24:55 -0700

    add 'questions' controller logic to save new questions; add 'questions' route to make 'question' store avaiable to controller/view; add temp display logic to 'questions' template to verify

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,7 +1,14 @@
-App.QuestionsController = Ember.Controller.extend({
+App.QuestionsController = Ember.ArrayController.extend({
   askingQuestion: false,
   actions: {
     askQuestion: function() {
+      var newQuestion = this.store.createRecord('question', {
+        question: this.get('question'),
+        author: this.get('author'),
+        description: this.get('description'),
+        date: new Date(),
+      });
+      newQuestion.save();
       this.set('askingQuestion', false);
     },
     showQuestionForm: function() {
